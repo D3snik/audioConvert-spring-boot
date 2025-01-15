2a460e912abc2f0da67651be8423e6dab582cad2
Ryan Linhares <ryandesnik45@gmail.com>
2025-01-15 04:22:19 -0300

    first commit

---
+++ src/main/java/com/d3snik/AudioConvert/AudioConvertApplication.java
@@ -0,0 +1,13 @@
+package com.d3snik.AudioConvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioConvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioConvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d3snik/AudioConvert/AudioConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3snik.AudioConvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
